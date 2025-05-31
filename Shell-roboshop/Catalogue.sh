6eb9eafafddbc23043aeba490895be2d27848eba
saitharun <saitharundevops.com>
2025-05-31 13:37:01 +0530

    commit

--- Shell-roboshop/Catalogue.sh
+++ Shell-roboshop/Catalogue.sh
@@ -78,7 +78,8 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
 STATUS=$(mongosh --host mongodb.busy98.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-if [ "$STATUS" -lt 0 ]; then
+if [ $STATUS -lt 0 ]
+then
     mongosh --host mongodb.busy98.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
