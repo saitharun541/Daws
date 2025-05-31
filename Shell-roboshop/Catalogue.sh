7ffbcb2f7110b693f2cbc2714e30152bc717319e
saitharun <saitharundevops.com>
2025-05-31 13:28:05 +0530

    commit

--- Shell-roboshop/Catalogue.sh
+++ Shell-roboshop/Catalogue.sh
@@ -77,10 +77,10 @@ cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongodb.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-STATUS=$(mongosh --host mongodb.busy98.site.com --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+STATUS=$(mongosh --host mongodb.busy98.site --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 if [ $STATUS -lt 0 ]
 then
-    mongosh --host mongodb.busy98.site.com </app/db/master-data.js &>>$LOG_FILE
+    mongosh --host mongodb.busy98.site </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
 else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
